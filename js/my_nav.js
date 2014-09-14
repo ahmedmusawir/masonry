8cf9185ea7d3fcb87e07134bf6bc036cd7a5a5c9
Da Moose <ahmed.musawir@hotmail.com>
2014-09-14 17:15:10 +0800

    responsive nav added - incomplete

--- js/my_nav.js
+++ js/my_nav.js
@@ -33,5 +33,42 @@ jQuery(document).ready(function($) {
 	});
 	// $('#nav-menu') Ends
 
+	var navClass = '.sub-menu', subOpenClass = 'sub-menu-open';
+
+	$('.menu-item-has-children').on('click', function (event) {
+
+		// $(this).children('.sub-menu').css('display', 'none');			
+		// alert('clicked');
+		if($(navClass).hasClass(subOpenClass)) {
+
+			$(navClass).animate({
+				height: 0
+			  },
+				300, function() {
+				setTimeout(function () {
+					// $(navClass).firstChild('li').css('border', '1px solid red');
+					$(navClass).removeClass(subOpenClass).removeAttr('style');
+				}, 320);
+			});
+		}
+		else {
+			// alert('close nav');
+			// $(navId).addClass(navOpenClass);
+
+			var newHeight = $(navClass).css('height', 'auto').height();
+			// alert(newHeight);
+			$(navClass).height(0).animate({
+				height: newHeight
+			},
+				300, function() {
+				/* stuff to do after animation is complete */
+				setTimeout(function () {
+					$(navClass).addClass(subOpenClass).removeAttr('style');
+				}, 320);
+			});
+		}
+	});
+	// $('.sub-menu') Ends
+
 });
 // jQuery(document) Ends
